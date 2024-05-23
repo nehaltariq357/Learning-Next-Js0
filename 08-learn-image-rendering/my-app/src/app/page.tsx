1e193e8b7c7b1a19c59553497ccb09bcfd2c37b2
Nehal <nehaltariq78@gmail.com>
2024-05-22 21:43:13 -0700

    learning

---
+++ 08-learn-image-rendering/my-app/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import nature from "../../image/nature.jpeg"
+export default function Home() {
+  return (
+   <>
+   <h1 className="text-center pt-5 text-[24px] font-bold 
+">Images in next js</h1>
+<Image className="rounded-full" src={nature} alt="nature"/>
+<br />
+<Image className="rounded-full" src={require("../../image/river.jpeg")} alt="river"/>
+   </>
+  );
+}
